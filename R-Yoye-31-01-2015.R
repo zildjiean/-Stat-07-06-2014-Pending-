6a4c0e3527794ebfe1096b8bdb1741ed58b4bb7b
zildjiean <piyapong-pp@hotmail.com>
2015-01-31 11:53:49 +0700

    Create R-Yoye-31-01-2015.R

---
+++ R-Yoye-31-01-2015.R
@@ -0,0 +1,106 @@
+function (m,n,p,alpha,m2,n2,p2,round1) 
+{
+	
+	#set.seed(5)
+	rlr=rep(0,round1)
+	rll=rep(0,round1)
+	x=rbinom(m,n,p)
+	z=length(x)
+	j=0
+	k=0
+
+		for(i in 1:z){
+			p[i]=(x[i]/n)
+		}
+		pbar=(sum(p)/m)
+		cat('\n','M =',p,' ','\n')
+		Mbar=pbar
+cat ('\n','M bar =',Mbar,'\n')
+
+####################### Calculate UCL,CL,LCL ##############################
+######## Calculate CAL(L) ##########
+	CLl = (Mbar*alpha)
+	LCLl= CLl-(3*(sqrt(((CLl)*(1-CLl))/n)))
+	UCLl= CLl+(3*(sqrt(((CLl)*(1-CLl))/n)))
+
+#cat('\n','--------------- Real UCL,CL,LCL for (L) ------------------','\n')
+#cat('\n','UCL(L) =',UCLl,'CL(L) =',CLl,'LCL(L) =',LCLl,'\n')
+
+
+######## Calculate CAL(R) ##########
+	CLr = 1-((1-((Mbar*alpha)*alpha)))
+	LCLr= CLr-(3*(sqrt(((CLr)*(1-CLr))/n)))
+	UCLr= CLr+(3*(sqrt(((CLr)*(1-CLr))/n)))
+
+#cat('\n','--------------- Real UCL,CL,LCL for (R) ------------------','\n')
+#cat('\n','UCL(R) =',UCLr,'CL(R) =',CLr,'LCL(R) =',LCLr,'\n')
+
+######## Check Condition for UCL,LCL (L) ##########
+		if(LCLl <= 0){
+			LCLl=0
+		} else LCLl=LCLl
+		if(UCLl > 1){
+			UCLl=1
+		} else UCLl=UCLl
+
+######## Check Condition for UCL,LCL (R) ##########
+		if(LCLr <= 0){
+			LCLr=0
+		}else LCLr=LCLr
+
+		if(UCLr > 1){
+			UCLr=1
+		} else UCLr=UCLr
+
+cat('\n','--------------- Calculate UCL,CL,LCL for (L) ------------------','\n')
+cat('\n','UCL(L) =',UCLl,'CL(L) =',CLl,'LCL(L) =',LCLl,'\n')
+cat('\n','--------------- Calculate UCL,CL,LCL for (R) ------------------','\n')
+cat('\n','UCL(R) =',UCLr,'CL(R) =',CLr,'LCL(R) =',LCLr,'\n')
+#cat('\n',"P = ",p,'\n')
+
+	for(i in 1:round1){
+		x2=rbinom(m2,n2,p2)
+		z1=length(x2)
+		pnew=rep(0,m2)
+
+
+	for (v in 1:z1){
+		pnew[v] = (x2[v]/n2)
+		L = pnew*alpha
+		R = 1-((1-pnew)*alpha)
+
+	}
+cat ('\n',"Pnew = ",pnew,'\n')
+cat ('\n',"L = ",L,'\n')
+cat ('\n',"R = ",R,'\n')
+
+		in.control = TRUE
+
+			while(in.control){
+
+				j=j+1
+				k=k+1
+				
+
+			
+				if((L[k]>UCLl)||(L[k]<LCLl))
+					{
+							in.control=FALSE
+					} 
+						rll[i] = j
+
+
+#				if((R[k]>UCLl)||(R[k]<LCLl))
+#					{
+#							in.control=FALSE
+#					} 
+#						rlr[i] = j						
+				}
+					
+		}
+
+	cat ('\n',"RL-R = ",rlr,"RL-L = ",rll,'\n')
+
+}
+
+#เหลือตัวเช็ค R ยังไม่เวิค
